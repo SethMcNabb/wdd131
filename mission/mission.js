2f128ab432d2840544168426b1a6d63e582d20d0
SethMcNabb <sethmcnabb00@gmail.com>
2024-05-04 17:40:38 -0600

    Completed Mission Statement part 2

---
+++ mission/mission.js
@@ -0,0 +1,16 @@
+const themeSelector = document.getElementById('light-or-dark');
+function changeTheme() {
+    const currentSetting = themeSelector.value;
+    
+    const body = document.body;
+    const byuiLogo = document.getElementById('logo');
+
+    if (currentSetting === 'dark') {
+        body.classList.add('dark');
+        byuiLogo.src = 'byui-logo_dark.png';
+    } else {
+        body.classList.remove('dark');
+        byuiLogo.src = 'byui-logo_blue.webp';
+    }
+}
+themeSelector.addEventListener('click', changeTheme);
